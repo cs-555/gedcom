5395cf1c5020c1f4e16988d5da7a792705210053
Joe <jsalomo1@gmail.com>
2013-10-22 01:28:19 -0700

    Added person and family outputs to console

--- netbeans_projects/gedcom/src/gedcom/OutputDisplayer.java
+++ netbeans_projects/gedcom/src/gedcom/OutputDisplayer.java
@@ -1,18 +1,47 @@
 package gedcom;
  
+import java.util.ArrayList;
 import java.util.List;
 
 public class OutputDisplayer {
 	
 	public static void displayData(List<Person> lPerson, List<Family> lFamily ){
 		
+		System.out.println("Person List:\n");
+        for ( int i = 0 ; i < lPerson.size() ; i++){
+        	Person person = lPerson.get(i);
+        	System.out.println(  
+        			person.getIndi() + ": " + person.getName() + 
+        			" (" + person.getBirt() + " - " + person.getDeat() + "), " + person.getSex()
+                    );
+        }
 	
-		//display person info using lPerson
-				
+        System.out.println("\nFamily List:\n");
+        for ( int i = 0 ; i < lFamily.size() ; i++){
+        	Family family = lFamily.get(i);
+        	System.out.println(
+                    family.getIdentifier() + ": " + 
+                    "Husband: " + findPeople(lPerson, family.getHusb()) + 
+                    "; Wife: " + findPeople(lPerson, family.getWife()) + 
+                    "; Married: " + family.getMarr() + 
+                    "; Divorced: " + family.getDiv() +
+                    "; Children: " + findPeople(lPerson, family.printAllChildren()) +
+                    "");
+        }
 		
-		//display family info using iFamily
+	}
+	
+	public static String findPeople(List<Person> lPerson, String index){
+		String names = "";
+		String[] indexes = index.trim().split("\\s+");
 		
+		for (int i = 0; i<indexes.length; i++){
+			for ( int j = 0 ; j < lPerson.size() ; j++){
+				if( lPerson.get(j).getIndi().trim().equals(indexes[i].trim()) ) names += lPerson.get(j).getName() + ", ";
+			}
+		}
 		
+		return names.trim();
 	}
 
 }
