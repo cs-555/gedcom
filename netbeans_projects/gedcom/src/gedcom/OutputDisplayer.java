2c3360b7a585292d6762bee1ef54998d8572fc97
Alan <Alan@192.168.1.107>
2013-11-01 12:56:17 -0400

    Updated to start coding for sprint2.  Adding code to support check death before marriage

--- netbeans_projects/gedcom/src/gedcom/OutputDisplayer.java
+++ netbeans_projects/gedcom/src/gedcom/OutputDisplayer.java
@@ -6,65 +6,73 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OutputDisplayer {
-    /** @author Joseph */
 
     public static void displayData(List<Person> lPerson, List<Family> lFamily ){
         Person personNode;
         Family familyNode;
+
+        printPersonNode(lPerson);
+        printFamilyNode(lFamily, lPerson);
+        printDeathBeforeMarriageErrors(lFamily, lPerson);
         
+    }
+
+    private static void printDeathBeforeMarriageErrors(List<Family> lFamily, List<Person> lPerson) {
+        Person personNode;
+        Family familyNode;
+        String Death;
+
+        for ( int i = 0 ; i < lFamily.size() ; i++){
+            familyNode = lFamily.get(i);
+            // using the husband ID get his death
+            Death = findPersonNode(lPerson, familyNode.getHusb()).toString();
+            poutln(Death);
+            // if death is before birth print HUSBerror
+            // using the wife ID get her death
+            // if death is before birth print WIFE error
+        }
+        poutln("\r\nDeath before Marriage test");
+    }
+        
+    private static void printFamilyNode(List<Family> lFamily, List<Person> lPerson) {
+        Family familyNode;
+        poutln("\r\nFamily List:\r\n");
+        for ( int i = 0 ; i < lFamily.size() ; i++){
+            familyNode = lFamily.get(i);
+            pout(familyNode.getIdentifier() + ": ");
+            pout("Husband: ");
+            pout(findPersonNode(lPerson, familyNode.getHusb()).getName() + ",");
+            pout("; Wife: ");
+            pout(findPersonNode(lPerson, familyNode.getWife()).getName() + ",");
+            pout("; Married: " + familyNode.getMarr());
+            if (!familyNode.getDiv().isEmpty()) {
+                pout("; Divorced: " + familyNode.getDiv());
+            }
+            if (familyNode.getAllChildren().size() > 0) {
+                pout("; Children: ");
+                for (int j = 0; j < familyNode.getAllChildren().size(); j++) {
+                    pout(findPersonNode(lPerson, familyNode.getAllChildren().get(j).toString()).getName() + ",");
+                    if (j < familyNode.getAllChildren().size() - 1) {
+                        pout(" ");
+                    }
+                }
+            }
+            pout("\r\n");
+        }
+    }
+
+    private static void printPersonNode(List<Person> lPerson) {
+        Person personNode;
         poutln("\r\nPerson List:\r\n");
         for ( int i = 0 ; i < lPerson.size() ; i++){
-        personNode = lPerson.get(i);
-        poutln(  personNode.getIndi() + ": " + personNode.getName() + 
-          " (" + personNode.getBirt() + " - " + personNode.getDeat() + "), " + personNode.getSex()
-        );
-    }
-
-    poutln("\r\nFamily List:\r\n");
-    for ( int i = 0 ; i < lFamily.size() ; i++){
-        familyNode = lFamily.get(i);
-//        OLD CODE: CAN BE DELETED AFTER MERGED
-//        poutln(
-//            familyNode.getIdentifier() + ": " + 
-//            "Husband: " + findPeople(lPerson, familyNode.getHusb()) + 
-            //"Husband: " + findPeople(lPerson, familyNode.getHusb()) + 
-//            "; Wife: " + findPeople(lPerson, familyNode.getWife()) + 
-//            "; Married: " + familyNode.getMarr() + 
-//            "; Divorced: " + familyNode.getDiv() +
-//            "; Children: " + findPeople(lPerson, familyNode.printAllChildren()) +
-//            ""
-//        );
-        pout(familyNode.getIdentifier() + ": ");
-        //  OLD CODE: pout("Husband: " + findPeople(lPerson, familyNode.getHusb()));
-        pout("Husband: ");
-        pout(findPerson(lPerson, familyNode.getHusb()).getName() + ",");
-        //  OLD CODE: pout("; Wife: " + findPeople(lPerson, familyNode.getWife()));
-        pout("; Wife: ");
-        pout(findPerson(lPerson, familyNode.getWife()).getName() + ",");
-        pout("; Married: " + familyNode.getMarr());
-        pout("; Divorced: " + familyNode.getDiv());
-        //  OLD CODE: poutln("; Children: " + findPeople(lPerson, familyNode.printAllChildren()));
-        pout("; Children: ");
-        //poutln(findPeople(lPerson, familyNode.printAllChildren()));
-        for (int j = 0; j < familyNode.getAllChildren().size(); j++) {
-            pout(findPerson(lPerson, familyNode.getAllChildren().get(j).toString()).getName() + ",");
-            // this is messy for now so the refactor is perfect
-            // previous routine did a trim so the last child shouldn't have a space after it
-            // but this can be changed when refactoring is done to clean it up
-            // previous routine also has an unnecessary ',' in the output after the last child
-            if (j < familyNode.getAllChildren().size() - 1) {
-                pout(" ");
-            }
+            personNode = lPerson.get(i);
+            poutln(  personNode.getIndi() + ": " + personNode.getName() +
+                    " (" + personNode.getBirt() + " - " + personNode.getDeat() + "), " + personNode.getSex()
+            );
         }
-        pout("\r\n");
     }
 
-    }
-
-    // REFACTORED: Wanted to use a single routine for finding a person node
-    // Will allow use of this routine when looking up dates for Death before Marriage
-    // previous findPeople was returning a string only of real use for outputting
-    public static Person findPerson(List<Person> lPerson, String ID) {
+    public static Person findPersonNode(List<Person> lPerson, String ID) {
         Person personNode;
         
         for ( int i = 0 ; i < lPerson.size() ; i++) {
@@ -76,19 +84,6 @@ public class OutputDisplayer {
         return personNode = null;
     }
     
-    //  OLD CODE: CAN BE DELETED AFTER MERGE
-    public static String findPeople(List<Person> lPerson, String index){
-            String names = "";
-            String[] indexes = index.trim().split("\\s+");
-
-            for (int i = 0; i<indexes.length; i++){
-                    for ( int j = 0 ; j < lPerson.size() ; j++){
-                            if( lPerson.get(j).getIndi().trim().equals(indexes[i].trim()) ) names += lPerson.get(j).getName() + ", ";
-                    }
-            }
-            return names.trim();
-    }
-
     // following 2 methods to allow multiple outputs w/single line throughout the program
     public static void pout(String output) {
         System.out.print(output);
@@ -108,5 +103,5 @@ public class OutputDisplayer {
             System.out.print("pout writer problem, please investigate");
         }
     }
-        
+
 }
