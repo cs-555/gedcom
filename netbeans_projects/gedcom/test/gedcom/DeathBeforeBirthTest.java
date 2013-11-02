327d7e958eb680968286d1750946c3a7f8750804
Michael Casey <mcasey8540@gmail.com>
2013-11-02 13:58:14 -0400

    Add DeathBeforeBirthTest test methods.

--- netbeans_projects/gedcom/test/gedcom/DeathBeforeBirthTest.java
+++ netbeans_projects/gedcom/test/gedcom/DeathBeforeBirthTest.java
@@ -6,10 +6,48 @@
 
 package gedcom;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  *
  * @author michaelcasey
  */
 public class DeathBeforeBirthTest {
+    
+    public DeathBeforeBirthTest() {
+        System.out.print("StartBeforeBirthTest");
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetDeathBeforeBirthErrors() {
+        System.out.println("getDeathBeforeBirthErrors");
+        DeathBeforeBirth instance = new DeathBeforeBirth();
+        String expResult = 3;
+        String result = instance.getDeathBeforeBirthErrors().size;
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }    
     
 }
