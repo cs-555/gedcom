95dccbd90128795e45b2a787745857aedca6d0e6
Michael Casey <mcasey8540@gmail.com>
2013-11-03 10:03:56 -0500

    Generate new DeathBeforeBirthTest class based on updated logic from DeathBeforeBirth class.

--- netbeans_projects/gedcom/test/gedcom/DeathBeforeBirthTest.java
+++ netbeans_projects/gedcom/test/gedcom/DeathBeforeBirthTest.java
@@ -6,6 +6,9 @@
 
 package gedcom;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,7 +23,6 @@ import static org.junit.Assert.*;
 public class DeathBeforeBirthTest {
     
     public DeathBeforeBirthTest() {
-        System.out.print("StartBeforeBirthTest");
     }
     
     @BeforeClass
@@ -39,17 +41,75 @@ public class DeathBeforeBirthTest {
     public void tearDown() {
     }
 
+    /**
+     * Test of getDeathBeforeBirthErrors method, of class DeathBeforeBirth.
+     */
     @Test
     public void testGetDeathBeforeBirthErrors() {
         System.out.println("getDeathBeforeBirthErrors");
-        DeathBeforeBirth instance = new DeathBeforeBirth();
-        
-        //test quantity of errors
-        Integer expNumberOfErrors = 3;
-        Integer result = instance.getDeathBeforeBirthErrors().size();
-        assertEquals(expNumberOfErrors, result);
+        List<Person> lperson = null;
+        ArrayList expResult = null;
+        ArrayList result = DeathBeforeBirth.getDeathBeforeBirthErrors(lperson);
+        assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
-    }    
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isBirthAfterDeath method, of class DeathBeforeBirth.
+     */
+    @Test
+    public void testIsBirthAfterDeath() {
+        System.out.println("isBirthAfterDeath");
+        GregorianCalendar birthDate = null;
+        GregorianCalendar deathDate = null;
+        boolean expResult = false;
+        boolean result = DeathBeforeBirth.isBirthAfterDeath(birthDate, deathDate);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of formatBirthdate method, of class DeathBeforeBirth.
+     */
+    @Test
+    public void testFormatBirthdate() {
+        System.out.println("formatBirthdate");
+        String lpersonBirthdate = "";
+        GregorianCalendar expResult = null;
+        GregorianCalendar result = DeathBeforeBirth.formatBirthdate(lpersonBirthdate);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of formatDeathdate method, of class DeathBeforeBirth.
+     */
+    @Test
+    public void testFormatDeathdate() {
+        System.out.println("formatDeathdate");
+        String lpersonDeathDate = "";
+        GregorianCalendar expResult = null;
+        GregorianCalendar result = DeathBeforeBirth.formatDeathdate(lpersonDeathDate);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMonthInteger method, of class DeathBeforeBirth.
+     */
+    @Test
+    public void testGetMonthInteger() {
+        System.out.println("getMonthInteger");
+        String month = "";
+        Integer expResult = null;
+        Integer result = DeathBeforeBirth.getMonthInteger(month);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
     
 }
